f2b1718f0cee03650ccf87d653c697d16c3a2af0
Julio Quiroz <chulini@gmail.com>
2019-06-10 23:04:35 +0200

    loading bconfirmed/unconfirmed balance, log window, copy mnemonic and address

---
+++ Assets/Scripts/AnimatedLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimatedLoader : MonoBehaviour
+{
+    [SerializeField] float RotationVel;
+    Transform myTransform;
+    void Awake()
+    {
+        myTransform = GetComponent<Transform>();
+    }
+    void Update()
+    {
+        myTransform.rotation *= Quaternion.Euler(0,0,Time.deltaTime*RotationVel);
+    }
+}
--- Assets/Scripts/BitcoinWallet.cs
+++ Assets/Scripts/BitcoinWallet.cs
@@ -12,11 +12,15 @@ public class BitcoinWallet : MonoBehaviour
 	[SerializeField] Button NewWalletButton;
 	[SerializeField] Text BalanceAmountText;//TODO load balance from api
 	[SerializeField] Button BalanceRefreshButton;//TODO Same
+	[SerializeField] GameObject BalanceAnimatedLoader; 
 	
 	[Header("Wallet Receive")]
 	[SerializeField] Text AddressText;
 	[SerializeField] RawImage AddressQRRawImage;
 	[SerializeField] Button AddressQRButton;//TODO click copy address
+	[SerializeField] Text CopyAddressButtonText;
+	[SerializeField] GameObject QRAnimatedLoader;
+	
 	
 	[Header("Wallet Send")]
 	[SerializeField] Button SendButton;//TODO send money
@@ -24,8 +28,12 @@ public class BitcoinWallet : MonoBehaviour
 	[Header("Private")]
 	[SerializeField] InputField MnemonicInputField;
 	[SerializeField] Button ImportButton;
-	[SerializeField] Button CopyMnemonicButton;//TODO copy menmonic onclick
+	[SerializeField] Button CopyMnemonicButton;
+	[SerializeField] Text CopyMnemonicButtonText;
 	[SerializeField] InputField PrivateKeyInputField;
+
+	[Header("Logs")]
+	[SerializeField] Text LogsText;
 	
 	Mnemonic m_WalletMnemonic;
 	public Mnemonic WalletMnemonic
@@ -38,9 +46,68 @@ public class BitcoinWallet : MonoBehaviour
 			{
 				m_WalletMnemonic = value;
 				RefreshWalletUI();
+				StartCoroutine(RefreshBalance());
 			}
 		}
 	}
+
+	//Balance JSON example:
+	//{
+	//	"address": "1DEP8i3QJCsomS4BSMY2RpU1upv62aGvhD",
+	//	"total_received": 4433416,
+	//	"total_sent": 0,
+	//	"balance": 4433416,
+	//	"unconfirmed_balance": 0,
+	//	"final_balance": 4433416,
+	//	"n_tx": 7,
+	//	"unconfirmed_n_tx": 0,
+	//	"final_n_tx": 7
+	//}
+	JSONObject m_balanceData;
+
+	int m_confirmedBalance
+	{
+		get
+		{
+			
+			int bal = 0;
+			if (m_balanceData != null && !m_balanceData.IsNull && m_balanceData.GetField(ref bal, "balance"))
+			{
+				return bal;
+			}
+			else
+			{
+				Log("Returning invalid m_confirmedBalance", true);
+			}
+			return int.MinValue;
+		}
+	}
+
+	int m_unconfirmedBalance
+	{
+		get
+		{
+			int bal = 0;
+			if (m_balanceData != null && !m_balanceData.IsNull && m_balanceData.GetField(ref bal, "unconfirmed_balance"))
+			{
+				return bal;
+			}
+			else
+			{
+				Log("Returning invalid m_unconfirmedBalance", true);
+			}
+			return int.MinValue;
+		}
+	}
+	
+	string m_address
+	{
+		get
+		{
+			return WalletMnemonic.DeriveExtKey().GetPublicKey().GetAddress(ScriptPubKeyType.Legacy, Network.Main).ToString();
+		}
+	}
+	
 
 	void Awake()
 	{
@@ -48,49 +115,142 @@ public class BitcoinWallet : MonoBehaviour
 		{
 			GenerateWallet();
 		});
-		
 		ImportButton.onClick.AddListener(() =>
 		{
 			GenerateWallet(MnemonicInputField.text);
+		});
+		BalanceRefreshButton.onClick.AddListener(() =>
+		{
+			StartCoroutine(RefreshBalance());
+		});
+		CopyMnemonicButton.onClick.AddListener(() =>
+		{
+			CopyTextToClipboard(MnemonicInputField.text);
+			StartCoroutine(Animatetext("Copied :D", CopyMnemonicButtonText));
+		} );
+		AddressQRButton.onClick.AddListener(() =>
+		{
+			CopyTextToClipboard(m_address);
+			StartCoroutine(Animatetext("Address copied :D", AddressText));
 		});
 	}
 
 	void Start()
 	{
-		GenerateWallet();
 		//position rough review helmet urban great custom carpet custom honey mango talent
+		GenerateWallet();
 	}
 
-	void GenerateWallet(string mnemonicStr = "")
-	{	
-		WalletMnemonic = (mnemonicStr == "") ? new Mnemonic(Wordlist.English, WordCount.Twelve) : new Mnemonic(mnemonicStr);		
-	}
-	
 	void RefreshWalletUI()
 	{
+		//INTERESTING every mnemonic can be derived to its equivalent private key
+		//Which is used to sign transactions
 		MnemonicInputField.text = WalletMnemonic.ToString();
 		PrivateKeyInputField.text = WalletMnemonic.DeriveExtKey().ToString(Network.Main);
-		string address = WalletMnemonic.DeriveExtKey().GetPublicKey().GetAddress(ScriptPubKeyType.Legacy, Network.Main).ToString();
-		AddressText.text = address;
-		
+		AddressText.text = m_address;
 		StopAllCoroutines();
-		StartCoroutine(LoadQR("bitcoin:"+address));
+		
+		//INTERESTING Mobile wallets uses "bitcoin:<address>" as the encoded string in QR images
+		StartCoroutine(LoadQR("bitcoin:"+m_address));
 	}
 
+	void Log(string newText, bool error = false)
+	{
+		if (error)
+			Debug.LogError(newText);
+		else
+			Debug.Log(newText);
+
+		LogsText.text += "\n> " + (error ? "<color=#ff0000ff><b>" : "") + newText + (error ? "</b></color>" : "");
+	}
+	
+	void GenerateWallet(string mnemonicStr = "")
+	{
+		if (mnemonicStr != "")
+		{
+			//INTERESTING mnemonic can be imported
+			Log("Importing wallet from mnemonic "+mnemonicStr.Substring(0,10)+"...");
+			WalletMnemonic = new Mnemonic(mnemonicStr);
+		}
+		else
+		{
+			Log("Generating new wallet");
+			//INTERESTING mnemonic can be randomly generated totally offline
+			WalletMnemonic = new Mnemonic(Wordlist.English, WordCount.Twelve);
+		}
+	}
+
+
+	IEnumerator RefreshBalance()
+	{
+		BalanceAnimatedLoader.SetActive(true);
+		BalanceRefreshButton.gameObject.SetActive(false);
+		BalanceAmountText.text = "Loading...";
+		yield return new WaitForSeconds(0.3f);
+		
+		//INTERESTING as the blockchain is public anybody can check the balance for any address
+		//In this case we are trusting an external company but this url request could be replaced by your own bitcoin node
+		WWW www = new WWW("https://api.blockcypher.com/v1/btc/main/addrs/"+m_address+"/balance");
+		// Documentation @ https://www.blockcypher.com/dev/bitcoin/#address-balance-endpoint
+		
+		yield return www;
+		if (www.error != null)
+		{
+			Log("Cannot get balance from \""+"\"\nError: "+www.error);
+			BalanceAmountText.text = "Couldn't refresh balance :C";
+		}
+		else
+		{
+			m_balanceData = new JSONObject(www.text);
+			Log("New balance data: "+m_balanceData.ToString(true));
+			BalanceAmountText.text = "Confirmed: "+m_confirmedBalance+"[sat]";
+			if (m_unconfirmedBalance != 0)
+				BalanceAmountText.text += "\t\tUnconfirmed: " + m_unconfirmedBalance;
+		}
+		BalanceAnimatedLoader.SetActive(false);
+		BalanceRefreshButton.gameObject.SetActive(true);
+	}
+	
+	
+	// Utilities
+	void CopyTextToClipboard(string txt)
+	{
+		TextEditor te = new TextEditor();
+		te.text = txt;
+		te.SelectAll();
+		te.Copy();
+	}
 	IEnumerator LoadQR(string str)
 	{
+		QRAnimatedLoader.SetActive(true);
+		AddressQRButton.gameObject.SetActive(false);
 		AddressQRRawImage.texture = null;
 		yield return new WaitForSeconds(0.3f);
 		WWW www = new WWW("http://chart.apis.google.com/chart?cht=qr&chs=300x300&chl="+str);
 		yield return www;
 		if (www.error != null)
 		{
-			Debug.LogError("Cannot get QR code from "+www.url+"\nError:"+www.error);
+			Log("Cannot get QR code from \"" + www.url + "\"\nError: " + www.error, true);
+			QRAnimatedLoader.SetActive(false);
+			AddressQRButton.gameObject.SetActive(false);
 		}
 		else
 		{
 			AddressQRRawImage.texture = www.texture;
+			QRAnimatedLoader.SetActive(false);
+			AddressQRButton.gameObject.SetActive(true);
 		}
-		
+	}
+
+	IEnumerator Animatetext(string newAnimatedText, Text uiText)
+	{
+		string lastText = uiText.text;
+		for (int i = 0; i <= newAnimatedText.Length; i++)
+		{
+			uiText.text = newAnimatedText.Substring(0, i);
+			yield return new WaitForSecondsRealtime(.03f);
+		}
+		yield return new WaitForSecondsRealtime(2.0f);
+		uiText.text = lastText;
 	}
 }
